df64775209dd243168c982f6b68783d03829b53c
Amin Khayam <aminkhayam@ip-172-16-188-69.eu-west-3.compute.internal>
2021-09-20 12:50:37 +0200

    login/registaration edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'auth', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: () => import('./auth/login-page/login-page.module').then(c => c.LoginPageModule) },
   { path: 'posts', loadChildren: () => import('./posts/list/main.module').then(c => c.MainModule) },
   { path: 'posts/:id', loadChildren: () => import('./posts/detail/describe.module').then(c => c.DescribeModule) },
