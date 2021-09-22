aefd729e3f07ed2e4b81681074eb6ea3fab66468
Amin Khayam <aminkhayam@ip-172-16-188-69.eu-west-3.compute.internal>
2021-09-22 12:11:28 +0200

    remove validators for password during login

--- src/app/auth/login/form/login.form.config.ts
+++ src/app/auth/login/form/login.form.config.ts
@@ -1,26 +1,17 @@
 import { FormBuilder, FormGroup } from "@angular/forms";
-import * as PasswordValidator from "src/app/_utils/components/form/validator/password.validator";
-import * as UsernameValidator from "src/app/_utils/components/form/validator/username.validator";
+import * as MailValidator from "src/app/_utils/components/form/validator/mail.validator";
 
 export const getLoginForm: (fb: FormBuilder) => FormGroup = fb => fb.group({
   email: [
     "", [
       // sync validators
-      UsernameValidator.MinLength(8),
+      MailValidator.CheckMail,
     ], [
       // async validators
     ],
   ],
   password: [
-    "", [
-      PasswordValidator.CheckMaius,
-      PasswordValidator.CheckMinus,
-      PasswordValidator.CheckNumber,
-      PasswordValidator.CheckSpace,
-      PasswordValidator.CheckSymbol,
-      PasswordValidator.CheckTick,
-      PasswordValidator.MinLength(8),
-    ],
+    ""
   ],
 }, {
   validators: []
